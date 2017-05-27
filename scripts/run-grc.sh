d3f51407ca8e0e74fdd5df1d3d9018bd46988d84
Christopher Friedt <chrisfriedt@gmail.com>
2017-05-27 14:00:58 -0400

    rebase off of master

--- scripts/run-grc.sh
+++ scripts/run-grc.sh
@@ -4,6 +4,19 @@ export APP_DIR="$(cd "$(dirname "${0}")"/../../../../; pwd)"
 
 source "${APP_DIR}"/Contents/MacOS/usr/bin/grenv.sh
 
+if [ ! -f "${APP_DIR}"/Contents/MacOS/usr/bin/${PYTHON} ]; then
+  cp $(which ${PYTHON}) "${APP_DIR}"/Contents/MacOS/usr/bin || exit 1
+fi
+
+cd "${APP_DIR}"/Contents/MacOS/usr/bin
+install_name_tool -add_rpath "${PWD}" ${PYTHON}  2>/dev/null
+find "${APP_DIR}" -name '*.so' -type f > /tmp/run-grc.sh.solist
+while read line; do
+  install_name_tool -add_rpath "${PWD}" "${line}" 2>/dev/null
+done < /tmp/run-grc.sh.solist
+rm -f /tmp/run-grc.sh.solist
+cd "${OLDPWD}"
+
 gnuradio-companion ${@} &
 
 exit 0
