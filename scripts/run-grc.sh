6bdf1565920daf57e44c1f555a348d12856d6931
David Holl <dholl@subspacedynamics.com>
2017-06-02 23:16:15 -0700

    run-grc.sh: osascript to report loading progress

--- scripts/run-grc.sh
+++ scripts/run-grc.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
+set -e
+
+show_fail_message () {
+	osascript -e 'display notification "Failed to start GNU Radio Companion.\nDetail: '"$*"'" with title "GNU Radio Companion"' > /dev/null 2>&1 || true
+	return 0
+}
+
+# If the exec fails for some reason, then our EXIT handler will get invoked.
+# At exit, run cleanup_script but let the exit status still be whatever the
+# shell was going to exit with:
+trap 'show_fail_message "exit status $?"' EXIT
+
+# If we get a signal such as SIGINT SIGQUIT or SIGTERM, then mask signal,
+# show_fail_message, restore default handler, and raise signal again to let it
+# propagate to whatever process started this script:
+trap '{ trap - EXIT ; trap "" INT  ; show_fail_message "received signal INT"  ; trap - INT  ; kill -s INT  $$ ; }' INT
+trap '{ trap - EXIT ; trap "" QUIT ; show_fail_message "received signal QUIT" ; trap - QUIT ; kill -s QUIT $$ ; }' QUIT
+trap '{ trap - EXIT ; trap "" TERM ; show_fail_message "received signal TERM" ; trap - TERM ; kill -s TERM $$ ; }' TERM
+# The leading "trap - EXIT" disables the EXIT signal handler or otherwise
+# show_fail_message gets called twice.
+
+# Uncomment one at a time to test each exit condition:
+#kill -s INT -- $$
+#kill -s QUIT -- $$
+#kill -s TERM -- $$
+#exit 2
+
 # Figure out where this script is located:
 case "$0" in
 	/*)
@@ -15,9 +42,36 @@ esac
 grenv_path="${argv0_path%/*}/grenv.sh"
 
 if ! test -e "${grenv_path}" ; then
+	osascript -e 'on run argv' -e 'display dialog ("Unable to find grenv.sh at " & item 1 of argv) buttons {"OK"} default button 1 with icon stop with title "GNU Radio Companion"' -e 'end run' "${grenv_path}" > /dev/null 2>&1 || true
 	printf 'Unable to find grenv.sh at %s\n' "${grenv_path}" 1>&2
 	exit 1
 fi
 . "${grenv_path}"
 
-exec gnuradio-companion "${@}"
+if command -v xset >/dev/null 2>&1 ; then
+	# If xset is available, then we'll use that to silently launch the X server.
+	# While we wait, use osascript to tell the user what we're up to.
+	osascript -e 'display notification "Starting X server..." with title "GNU Radio Companion"' > /dev/null 2>&1 || true
+	if xset q >/dev/null 2>&1 ; then
+		osascript -e 'display notification "Starting GNU Radio Companion..." with title "GNU Radio Companion"' > /dev/null 2>&1 || true
+	else
+		osascript -e 'display notification "Unable to verify running X server.  Will attempt anyway..." with title "GNU Radio Companion"' > /dev/null 2>&1 || true
+	fi
+else
+	osascript -e 'display notification "Starting GNU Radio Companion..." with title "GNU Radio Companion"' > /dev/null 2>&1 || true
+fi
+
+## Strip out the -psn_... argument added by the OS.
+#if [ "x${1#-psn_}" != "x${1}" ]; then
+#	shift 1
+#fi
+
+# Prime the exit status with "( exit 127 ) || " in case exec fails.  Otherwise,
+# $? is 0 during trap EXIT.
+( exit 127 ) || exec gnuradio-companion "${@}"
+# Without the "trap ... EXIT", then we wouldn't need the "( exit 127 ) || ",
+# since the shell would automatically return the appropriate error number from
+# exec's failure.  Is this a glitch in bash since sh is symlinked to bash on my
+# system?  Note that exec can return other exit status beside 127, so this
+# stunt is suboptimal, but an optimally pedantic solution would require exec to
+# interact better wirth trap EXIT.
